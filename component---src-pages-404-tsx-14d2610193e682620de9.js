6f7b4735e34848daf8b1c220c815757b238f3415
Andrew Ng <ayn@andrewng.com>
2022-03-06 19:02:33 -0800

    Updates

---
+++ component---src-pages-404-tsx-14d2610193e682620de9.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_blog_stater_with_typescript=self.webpackChunkgatsby_blog_stater_with_typescript||[]).push([[218],{9786:function(t,e,a){"use strict";a.r(e);var s=a(7294),n=a(5962),l=a(2740);e.default=function(t){var e=t.location;return s.createElement(n.Z,{location:e},s.createElement(l.Z,{title:"404: Not Found"}),s.createElement("h1",{className:"h1"},"404: Not Found"),s.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-14d2610193e682620de9.js.map
